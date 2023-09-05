8125328d485cd22508abb00523eb76f30c5791fe
MathiasVlff <mathiasvelicofft@gmail.com>
2023-09-05 14:55:42 -0400

    practice

---
+++ javascripts/validation.js
@@ -0,0 +1,35 @@
+function emailValidation() {
+	const form = document.getElementById("form");
+	const emailComfirmField = document.getElementById("email_confirm");
+	const element = document.createElement("p");
+	const message = document.createTextNode("Correo electrónico no coincidente.");
+	const contentField = document.getElementById("content_field");
+	element.appendChild(message);
+	element.setAttribute("id", "alert");
+	element.classList.add("alert_color");
+	emailComfirmField.addEventListener("input", (e) => {
+		if (form.email.value !== form.email_confirm.value) {
+			if (!document.getElementById("alert")) {
+				contentField.parentNode.insertBefore(element, contentField);
+				emailComfirmField.classList.add("alert_bg");
+			}
+		} else {
+			emailComfirmField.classList.remove("alert_bg");
+			element.parentNode.removeChild(element);
+		}
+	});
+}
+
+window.onload = function () {
+	emailValidation();
+};
+
+const app = new Vue({
+  el: '#app',
+  data: {
+    errorMessage: 'Correo electrónico no coincidente.',
+		formData: {
+			name: '',
+		},
+  }
+})
